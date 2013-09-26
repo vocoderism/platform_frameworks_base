8b776c5f6812728290c3a671873862a8e5b346c9
htchoi1995 <htchoi1995@gmail.com>
2013-09-26 13:01:58 +0200

    [1/2] SystemUI: Network speed indicator (Squshed)
    Change-Id: I88e7cdfe20342c695a33383db36bf80bb4ffe009
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/com/android/internal/logging/MetricsConstants.java
    	packages/SystemUI/res/values/chroma_dimens.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java

--- core/java/com/android/internal/logging/MetricsConstants.java
+++ core/java/com/android/internal/logging/MetricsConstants.java
@@ -263,6 +263,7 @@ public interface MetricsConstants {
     public static final int TUNER_BATTERY_PERCENTAGE = 237;
     public static final int FUELGAUGE_INACTIVE_APPS = 238;
     public static final int BUTTONS = 239;
+    public static final int NETWORK_TRAFFIC_SETTINGS= 240;
 
     // These constants must match those in the analytic pipeline, do not edit.
     // Add temporary values to the top of MetricsLogger instead.
