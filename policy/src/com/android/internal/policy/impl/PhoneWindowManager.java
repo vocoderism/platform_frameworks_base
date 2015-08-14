3b550b1fb6aab0755158bee391ee806b78e93939
ghbhaha <ghbhaha@gmail.com>
2015-08-15 01:10:41 +0800

    fix three_finger_gesture
    Change-Id: I37d736c3e9cfdf486e6f615dec33a8c7beadab42

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1438,6 +1438,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             haveEnableGesture = true;
             mWindowManagerFuncs.registerPointerEventListener(mOPGestures);
         } else {
+            if (!haveEnableGesture) return;
             haveEnableGesture = false;
             mWindowManagerFuncs.unregisterPointerEventListener(mOPGestures);
         }
