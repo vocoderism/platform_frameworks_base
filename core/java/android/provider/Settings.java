7843f2c85b0bc34166012bbc881d335810e9dc47
LorDClockaN <davor@losinj.com>
2015-08-28 20:59:06 +0200

    Toast icon switch [1/2]
    - Turn off by default
    
    Change-Id: I901b7af8f9d574b82eb6c0c0128ff7c7c2eed1be

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3373,6 +3373,13 @@ public final class Settings {
 	public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
 
         /**
+         * Toast icon
+         *
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
