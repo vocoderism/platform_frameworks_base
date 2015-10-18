2e8a928e19232d186896fa887013b4ad6e8e6b72
Idrees Dargahwala <dargahidrees@gmail.com>
2015-10-18 16:42:39 +0530

    Fix this Build
    Change-Id: I0c581f06928a66789ece96ffadf6d52526aa5b30

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -745,6 +745,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ACCELEROMETER_ROTATION_ANGLES), false, this,
 		    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_HEIGHT), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
